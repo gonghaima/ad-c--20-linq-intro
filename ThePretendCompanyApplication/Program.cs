d93c29cb68987652d0a0fd1e609244d22a35ab11
Steven Gong <gonghaima@hotmail.com>
2024-03-31 09:18:59 +1100

    extention filter on department data, lambda expression for both Filter extension and linQ

--- ThePretendCompanyApplication/Program.cs
+++ ThePretendCompanyApplication/Program.cs
@@ -11,16 +11,31 @@ class Program
     static void Main(string[] args)
     {
         List<Employee> employeeList = Data.GetEmployees();
-        var filteredEmployees = employeeList.Filter(emp => emp.IsManager == true );
+        // var filteredEmployees = employeeList.Filter(emp => emp.IsManager == true );
+        // var filteredEmployees = employeeList.Filter(emp => emp.AnnualSalary < 50000);
 
-        foreach (var employee in filteredEmployees)
+        // foreach (var employee in filteredEmployees)
+        // {
+        //     Console.WriteLine($"First Name: {employee.FirstName}");
+        //     Console.WriteLine($"Last Name: {employee.LastName}");
+        //     Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+        //     Console.WriteLine($"Manager: {employee.IsManager}");
+        //     Console.WriteLine();
+        // }
+
+        List<Department> departmentList = Data.GetDepartments();
+
+        // var filteredDepartments = departmentList.Where(dept => dept.ShortName == "TE" || dept.ShortName == "HR");
+        var filteredDepartments = departmentList.Filter(dept => dept.Id > 1);
+        foreach (var department in filteredDepartments)
         {
-            Console.WriteLine($"First Name: {employee.FirstName}");
-            Console.WriteLine($"Last Name: {employee.LastName}");
-            Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
-            Console.WriteLine($"Manager: {employee.IsManager}");
+            Console.WriteLine($"Id: {department.Id}");
+            Console.WriteLine($"Short Name: {department.ShortName}");
+            Console.WriteLine($"Long Name: {department.LongName}");
             Console.WriteLine();
         }
+
+
         Console.ReadLine();
     }
 }
