1e24918332644076b19e147d45879e74a017fc86
Steven Gong <gonghaima@hotmail.com>
2024-03-30 13:31:06 +1100

    use filter extension

--- ThePretendCompanyApplication/Program.cs
+++ ThePretendCompanyApplication/Program.cs
@@ -1,10 +1,27 @@
-﻿ namespace ThePretendCompanyApplication;
+﻿using System;
+using System.Collections.Generic;
+using TCPData;
+using TCPExtensions;
+using System.Linq;
+
+namespace ThePretendCompanyApplication;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Employee> employeeList = Data.GetEmployees();
+        var filteredEmployees = employeeList.Filter(emp => emp.IsManager == true );
+
+        foreach (var employee in filteredEmployees)
+        {
+            Console.WriteLine($"First Name: {employee.FirstName}");
+            Console.WriteLine($"Last Name: {employee.LastName}");
+            Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+            Console.WriteLine($"Manager: {employee.IsManager}");
+            Console.WriteLine();
+        }
+        Console.ReadLine();
     }
 }
 
